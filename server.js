f9f17db577e64e6128572b3e2ea895c31fd76c0e
Vishal Rana <vishal@qwata.com>
2013-03-12 13:57:46 -0700

    Added port forwarding feature

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var http = require("http");
+var net = require("net");
+var util = require("util");
+var async = require("async");
+var _ = require("lodash");
 var scale = require("./scale");
 var logger = require("./scale").logger;
 var cfg = require("./scale").config;
@@ -12,5 +16,28 @@ server.on("error", function(err) {
 });
 
 server.on("listening", function() {
-  logger.info("scale started on port " + cfg.port);
+  // Port forwarding
+  async.parallel(_.map(cfg.forward, function(fwd) {
+    return function(cb) {
+      var server = net.createServer(function(srv) {
+        var clt = net.connect(fwd.target.port);
+        srv.pipe(clt);
+        clt.pipe(srv);
+      });
+
+      server.listen(fwd.port, function() {
+        logger.info(util.format("forwarded port %d => %s:%d", fwd.port, fwd.target.hostname, fwd.target.port));
+        cb();
+      });
+
+      server.on("error", function(err) {
+        cb(err);
+      });
+    };
+  }), function(err) {
+    if (err) {
+      return logger.error(err);
+    }
+    logger.info("scale started on port " + cfg.port);
+  });
 });
